4b6a4d4d7fa3cecf60dc88c57849671c8b846c76
Laurence Santos <lsantos@highlysucceed.com>
2023-07-11 14:28:29 +0800

    custom theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import customTheme from './customTheme.jsx'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -12,8 +13,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ChakraProvider theme={customTheme}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
