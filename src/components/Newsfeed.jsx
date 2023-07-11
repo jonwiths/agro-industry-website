2a112f0ae2ad5d5cc35a531e4d149cf4580ce3d8
Jonslogar Arenque <jonwiths@gmail.com>
2023-07-11 13:27:23 +0800

    first commit

---
+++ src/components/Newsfeed.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Newsfeed = () => {
+  return <div>Newsfeed</div>;
+};
+
+export default Newsfeed;
---
+++ src/components/OurSector.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OurSector = () => {
+  return <div>OurSector</div>;
+};
+
+export default OurSector;
