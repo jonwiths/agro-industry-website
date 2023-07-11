987a16fc3364a038f24ca939efa5a207b84e9bc0
Jonslogar Arenque <jonwiths@gmail.com>
2023-07-11 15:49:42 +0800

    created TopInfo.jsx, download react-icons and others

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import TopInfo from '../components/TopInfo';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import AboutUs from '../components/AboutUs';
+import { Accordion } from '@chakra-ui/accordion';
+import OurProjects from '../components/OurProjects';
+import Carousel from '../components/Carousel';
+import Vision from '../components/Vision';
+import OurSector from '../components/OurSector';
+import Media from '../components/Media';
+import Newsfeed from '../components/Newsfeed';
+import ContactUs from '../components/ContactUs';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <>
+      <TopInfo />
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <Carousel />
+      <OurProjects />
+      <Accordion />
+      <Vision />
+      <OurSector />
+      <Media />
+      <Newsfeed />
+      <ContactUs />
+      <Footer />
+    </>
+  );
 };
 
 export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from './theme.fonts';
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
