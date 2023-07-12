fc12d72d3503918e798e0dc32ca540ec1d230d72
Laurence Santos <lsantos@highlysucceed.com>
2023-07-12 19:48:44 +0800

    media

--- src/components/Newsfeed.jsx
+++ src/components/Newsfeed.jsx
@@ -7,7 +7,7 @@ const Newsfeed = () => {
       as='section' 
       display={'flex'} 
       justifyContent={'center'}
-      mb={'250px'}
+      mb={'280px'}
     >
       <Flex
         w={'1239px'}
@@ -21,6 +21,7 @@ const Newsfeed = () => {
             color={'#024731'} 
             fontSize={'50px'} 
             fontWeight={'400'}
+            textShadow={'0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'}
             >NEWSFEED
           </Heading>
           <Box bg={'#93CE20'} h={'8px'} w={'86px'}/>
@@ -48,43 +49,42 @@ const Newsfeed = () => {
               <iframe 
                 width="422px" 
                 height="198.165px" 
-                src="https://www.youtube.com/embed/vXVSw7wTsso" 
-                title="YouTube video player"
+                src="https://www.youtube.com/embed/cH1Q868APLg" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen
                 style={{
                   borderRadius: '14.436px', 
                   boxShadow: '0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'
                 }}
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                allowfullscreen
               >
               </iframe>
               <iframe 
                 width="422px" 
-                height="198.165px" 
-                src="https://www.youtube.com/embed/vXVSw7wTsso" 
-                title="YouTube video player"
+                height="198.165px"  
+                src="https://www.youtube.com/embed/hC4lemk_YqE" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen
                 style={{
                   borderRadius: '14.436px', 
                   boxShadow: '0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'
                 }}
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                allowfullscreen
-              >
-              </iframe>
+              ></iframe>
               <iframe 
                 width="422px" 
-                height="198.165px" 
-                src="https://www.youtube.com/embed/vXVSw7wTsso" 
-                title="YouTube video player"
+                height="198.165px"  
+                src="https://www.youtube.com/embed/qKoJCT-EPCg" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen
                 style={{
                   borderRadius: '14.436px', 
                   boxShadow: '0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'
                 }}
-                frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                allowfullscreen
               >
               </iframe>
             </VStack>
