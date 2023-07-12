8095405e5a955923c35c7268bf63d43c6515d67e
Laurence Santos <lsantos@highlysucceed.com>
2023-07-12 18:09:44 +0800

    Newsfeed

--- src/components/Newsfeed.jsx
+++ src/components/Newsfeed.jsx
@@ -1,14 +1,97 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Flex, Grid, GridItem, HStack, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 const Newsfeed = () => {
   return(
-    <Box as='section'>
-      <Box>
-        <Heading
-          color={'#024731'}
-          >NEWSFEED</Heading>
-      </Box>
+    <Box 
+      as='section' 
+      display={'flex'} 
+      justifyContent={'center'}
+      mb={'250px'}
+    >
+      <Flex
+        w={'1239px'}
+        h={'100vh'}
+        flexDirection={'column'}
+        alignItems={'flex-start'}
+        gap={'49px'}
+      >
+        <Box>
+          <Heading
+            color={'#024731'} 
+            fontSize={'50px'} 
+            fontWeight={'400'}
+            >NEWSFEED
+          </Heading>
+          <Box bg={'#93CE20'} h={'8px'} w={'86px'}/>
+        </Box>
+      <Grid templateColumns={'2, 1fr'}>
+        <HStack spacing={10}>
+          <GridItem colSpan={1}>
+            <iframe 
+              width="770px" 
+              height="634px" 
+              src="https://www.youtube.com/embed/vXVSw7wTsso" 
+              title="YouTube video player" 
+              frameborder="0"
+              style={{
+                borderRadius: '14.436px', 
+                boxShadow: '0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'
+              }}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowfullscreen
+            >
+            </iframe>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <VStack spacing={'19.7525px'}>
+              <iframe 
+                width="422px" 
+                height="198.165px" 
+                src="https://www.youtube.com/embed/vXVSw7wTsso" 
+                title="YouTube video player"
+                style={{
+                  borderRadius: '14.436px', 
+                  boxShadow: '0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'
+                }}
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen
+              >
+              </iframe>
+              <iframe 
+                width="422px" 
+                height="198.165px" 
+                src="https://www.youtube.com/embed/vXVSw7wTsso" 
+                title="YouTube video player"
+                style={{
+                  borderRadius: '14.436px', 
+                  boxShadow: '0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'
+                }}
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen
+              >
+              </iframe>
+              <iframe 
+                width="422px" 
+                height="198.165px" 
+                src="https://www.youtube.com/embed/vXVSw7wTsso" 
+                title="YouTube video player"
+                style={{
+                  borderRadius: '14.436px', 
+                  boxShadow: '0px 3.4345476627349854px 7.230626583099365px 0px rgba(0, 0, 0, 0.04), 0px 27.429088592529297px 57.745452880859375px 0px rgba(0, 0, 0, 0.07)'
+                }}
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen
+              >
+              </iframe>
+            </VStack>
+          </GridItem>
+        </HStack>
+      </Grid>
+      </Flex>
     </Box>
   );
 };
