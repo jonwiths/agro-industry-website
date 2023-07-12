ce9a2e62730fe6decc1011d93d12f2e759ddf665
Laurence Santos <lsantos@highlysucceed.com>
2023-07-12 15:23:39 +0800

    minor width bug

--- src/components/Newsfeed.jsx
+++ src/components/Newsfeed.jsx
@@ -1,7 +1,16 @@
+import { Box, Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const Newsfeed = () => {
-  return <div>Newsfeed</div>;
+  return(
+    <Box as='section'>
+      <Box>
+        <Heading
+          color={'#024731'}
+          >NEWSFEED</Heading>
+      </Box>
+    </Box>
+  );
 };
 
 export default Newsfeed;
